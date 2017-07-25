050f4ce7e104b4d3824114a22ba02b27f5687c19
Nick Parsons <nparsons08@gmail.com>
2017-07-25 16:47:47 -0600

    Cleaned up code & updated node modules

--- node/nodeSkeletonHandler.js
+++ node/nodeSkeletonHandler.js
@@ -1,47 +1,58 @@
-'use strict';
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
+// Module dependencies
+const MongoClient = require('mongodb').MongoClient;
 
 // Atlas connection outside of Lambda handler
-const atlas_uri = ">>> Atlas conn string here <<<"; // to-do read from AWS keys
+//const atlasUri = 'YOUR_ATLAS_CONNECTION_STRING_HERE'; // to-do read from AWS keys
+const atlasUri = 'mongodb://nick:ni05pa904427*@free-shard-00-00-zftrn.mongodb.net:27017,free-shard-00-01-zftrn.mongodb.net:27017,free-shard-00-02-zftrn.mongodb.net:27017/todos?ssl=true&replicaSet=FREE-shard-0&authSource=admin'; // to-do read from AWS keys
 
-let cachedDb = null;
+// Cache DB connection for future use
+let cachedDb;
 
-exports.handler = (event, context, callback) => {
+// AWS event handler
+exports.handler = (evt, ctx, cb) => {
+
     // Set to false to allow re-use of database connections across lambda calls
-    // see http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
-    context.callbackWaitsForEmptyEventLoop = false;
+    // See http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+    ctx.callbackWaitsForEmptyEventLoop = false;
+
+    // Executes query
+    function executeQuery(db, query, cb) {
+
+        // Find documents in tasks collection
+        db.collection('tasks').find(query).toArray(function(err, docs) {
+            if (err) process.exit(1);
+            cb(null, docs);
+        });
+
+    }
 
     try {
-        if (cachedDb && cachedDb.serverConfig.isConnected()) {
-            //Execute Query
-            executeQuery(cachedDb, {}, callback);
-        }
-        else {
-            console.log("=== CONNECTING TO MONGODB ATLAS ==="); // Log to CloudWatch
-            MongoClient.connect(atlas_uri, function (err, db) {
+
+        if (!cachedDb) {
+
+            console.log(`=== CONNECTING TO MONGODB ATLAS ===`);
+
+            MongoClient.connect(atlasUri, (err, db) => {
+
                 if (err) {
-                    console.log(`the error is ${err}.`, err);
+                    console.error(`An error occurred! ${err}`);
                     process.exit(1);
                 }
-                cachedDb = db;
-                //Execute Query
-                executeQuery(cachedDb, {}, callback);
-            });
-        }
-    }
-    catch (err) {
-        console.error('an error occurred', err);
-        callback(err, null);
-    }
-}
 
-function executeQuery(db, jsonCriteria, callback) {
-  var collection = db.collection('MyColl');
-  // Find some documents
-  collection.find(jsonCriteria).toArray(function(err, docs) {
-    assert.equal(err, null);
-    callback(null, docs);
-  });
+                // Assign db connection to variable for further use
+                cachedDb = db;
+
+                // Execute query
+                executeQuery(cachedDb, {}, cb);
+
+            });
+
+        } else {
+            executeQuery(cachedDb, {}, cb);
+        }
+
+    } catch (err) {
+        console.error(`An error occurred! ${err}`);
+    }
+
 }
